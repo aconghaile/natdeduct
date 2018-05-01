8db67cdf89e3c7d5b7505c9ae8da905f6aa779b9
aconghaile <adam.peter.connolly@gmail.com>
2018-05-01 23:17:37 +0100

    The Parser Works Now!
    May still be a few bugs but for now we've got a way to parse from text into Proposition data type. I'd consider that a success for now.
    Stilll need to implement NOTs or _`¦_'s here but we could start implementing the indux/redux rules maybe in a new file?

--- natdeduct.hs
+++ natdeduct.hs
@@ -1,26 +1,52 @@
 data Logical = And | Or | Implies
 
-data Proposition = String | Proposition Logical Proposition
+data Proposition = Atomic String | Compound Proposition Logical Proposition
 
---parse :: [Char] -> Proposition
---parse x = if (noneSpecial x)
---		then Proposition x
---		else  
+instance Show Proposition where
+	show (Atomic str) = str
+	show (Compound a log b) = "(" ++ (show a) ++ (show log) ++ (show b) ++ ")"
 
-noneSpecial :: [Char] -> Bool
+instance Show Logical where
+	show And = " AND "
+	show Or = " OR "
+	show Implies = " IMPLIES "
+
+parse :: String -> Proposition
+parse [] = error "Empty Atomic"
+parse ('(':xs) = combine (bracket 1 [] xs)     
+parse x = if (noneSpecial x)
+		then Atomic x
+		else combine (split special [] x) 
+
+combine :: (String, String, String) -> Proposition
+combine ([], [], [])  = error "Empty Logical"
+combine (a, ")", []) = parse a
+combine (a, ")", b) = combine (a, [head b], (tail b))
+combine (a, x, b) = Compound (parse a) (toLogical x) (parse b)
+
+
+
+toLogical :: String -> Logical
+toLogical [] = error "Empty Logical"
+toLogical "n" = And
+toLogical "u" = Or
+toLogical ">" = Implies   
+
+noneSpecial :: String -> Bool
 noneSpecial x = foldl (&&) True (map (not) (map (special) x))
 
 special :: Char -> Bool
-special x = x `elem` ['u','n','>']
+special x = x `elem` ['u','n','>', ')']
 
-bracket :: [Char] -> [[Char]]
-bracket [] = []
-bracket x = if (head x == '(')
-	then split (')'==) [] (tail x)
-	else [x]
+bracket :: Integer -> String -> String -> (String, String, String) 
+bracket _ _ []  = error "Mismatched Brackets"
+bracket z x ('(':ys) = bracket (z+1) (x++"(") (ys)
+bracket 1 x (')':ys) = (x, ")", ys)
+bracket z x (')':ys) = bracket (z-1) (x++")") (ys)
+bracket z x (y:ys) = bracket z (x++[y]) ys
 
-split :: (Char -> Bool) -> [Char] -> [Char] -> [[Char]]
-split c x [] = [x]
+split :: (Char -> Bool) -> String -> String -> (String, String, String)
+split c x [] = ("", x, "")
 split c x y = if (c (head y))
-	then [x , [head y], tail y]
+	then (x , [head y], tail y)
 	else split c (x ++ [(head y)]) (tail y)
